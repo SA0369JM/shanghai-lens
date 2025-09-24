30dba156c6a2e941e99d2c4980c3e7b168fad8d5
AmyS <jshi0843@uni.sydney.edu.au>
2025-09-24 21:42:10 +1000

    Update all page

--- pages/banner.py
+++ pages/banner.py
@@ -4,12 +4,17 @@ st.title("Banner")
 
 st.subheader("🌟 Concept")
 st.write('''
-         This is a small, muted caption (good for footnotes).
+         The banner serves as a visual introduction to attract attention and immediately convey the cultural identity of Shanghai.
          ''')
+st.write ('''
+          It captures the city’s essence through its iconic night view: a dark background with the moon sets the atmosphere, while bright contrasting tones highlight vibrancy. 
+          Featuring the Bund’s four landmarks—the design guides the audience into Shanghai’s blend of tradition and modernity, making the banner both striking and memorable.
+ 
+          ''')
 
 st.subheader("🪧 Slogan")
 st.write ('''
-          - "more than just scenery": 
+          - "more than just scenery": Highlights that the city offers more than what meets the eye—its history, lifestyle, and spirit go beyond the surface of visual beauty, inviting people to truly experience its essence.
           ''')
 
 st.divider()
--- pages/poster.py
+++ pages/poster.py
@@ -6,7 +6,20 @@ import time
 st.title('Poster')
 
 st.subheader("🌟 Concept")
-st.write("This is a small, muted caption (good for footnotes).")
+st.write('''
+         The poster functions as a promotional medium to showcase the cultural and modern charm of Shanghai, attracting audiences' attention while conveying both its iconic landmarks and local lifestyle.
+         ''')
+st.write('''
+         The design integrates multiple symbolic elements:
+         - Skyline: The Oriental Pearl Tower, Jin Mao Tower, World Financial Center, and Shanghai Tower represent Shanghai's global image and modernity. It is also echoes the banner.
+         - Traditional Architecture: The illustration of classical pavilion structures connects the viewer with Shanghai's historical roots and cultural identity.
+         - Food Elements: Steamed buns (xiaolongbao) and hairy crabs highlight the city's rich culinary culture, a vital part of local life.
+         ''')
+
+st.subheader("🪧 Slogan")
+st.write ('''
+          - "Through the Lens of Shanghai, More Than Scenery": emphasizes that Shanghai is not only about its breathtaking views but also about culture, traditions, and lived experiences.
+          ''')
 
 st.divider()
 
